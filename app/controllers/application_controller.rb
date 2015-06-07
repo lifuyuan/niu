1dd868b6185b30d9cc8aa433bdb00cae147e5f96
lifuyuan <lifuyuan33@gmail.com>
2015-06-07 13:29:45 +0800

    handle exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :handle_intercept
+
+  rescue_from Exception do |exception|
+  	logger.error "发生异常，跳转，异常原因为：#{exception.message}"
+    logger.error exception.backtrace.inspect
+    redirect_to spree.root_path, notice: "系统异常"
+  end
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :grade,:password, :password_confirmation) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
+  end
+
+  private
+  def handle_intercept
+  	redirect_to root_path, notice: "系统暂不开放注册" if "/users/sign_up" == request.path
   end
 end
