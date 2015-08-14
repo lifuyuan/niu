ca18ae0a561724e34467a70470116198cd78a795
lifuyuan <lifuyuan33@gmail.com>
2015-08-14 11:30:07 +0800

    n

--- app/controllers/apk_controller.rb
+++ app/controllers/apk_controller.rb
@@ -1,10 +1,10 @@
 class ApkController < ApplicationController
   def download
-  	ApkDownload.create(id: request.remote_ip)
-  	send_file('files/app-release.apk',:filename => "app-release.apk", :disposition =>'attachment', :encoding => 'utf-8', :stream => false)
+  	ApkDownload.create(ip: request.remote_ip)
+  	send_file('public/app-release.apk',:filename => "app-release.apk", :disposition =>'attachment', :encoding => 'utf-8', :stream => false)
   end
 
   def upgrade
-  	send_data File.read('files/app-release.apk'), filename: "app-release.apk"
+  	send_data File.read('public/app-release.apk'), filename: "app-release.apk"
   end
 end
